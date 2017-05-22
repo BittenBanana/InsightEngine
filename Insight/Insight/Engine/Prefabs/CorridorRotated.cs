b556e2a037a16fca1a5677c22db8e1beb3d38abb
Marcin <jamesjohny11@gmail.com>
2017-05-23 00:48:21 +0200

    Added new prefabs

---
+++ Insight/Insight/Engine/Prefabs/CorridorRotated.cs
@@ -0,0 +1,49 @@
+﻿using Insight.Engine.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insight.Engine.Prefabs
+{
+    class CorridorRotated : Prefab
+    {
+        GameObject corridorModel;
+        GameObject floor;
+
+        public override void Initialize(Vector3 position)
+        {
+            prefabGameObjects = new List<GameObject>();
+
+            corridorModel = new GameObject(new Vector3(0, 0, 0), false);
+            floor = new GameObject(new Vector3(0, -3, 0), false);
+
+
+
+            corridorModel.AddNewComponent<MeshRenderer>();
+            floor.AddNewComponent<MeshRenderer>();
+            floor.GetComponent<MeshRenderer>().IsVisible = false;
+
+
+
+            prefabGameObjects.Add(corridorModel);
+            prefabGameObjects.Add(floor);
+            base.Initialize(position);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+
+            corridorModel.GetComponent<MeshRenderer>().Load(content, "Models/Konrads/Enviroment/cor-str-rt-g-rotated", 1.0f);
+            floor.GetComponent<MeshRenderer>().Load(content, "Models/Konrads/Enviroment/floorPlane", 1.0f);
+
+            corridorModel.AddNewComponent<BoxCollider>();
+            floor.AddNewComponent<BoxCollider>();
+            floor.physicLayer = Layer.Ground;
+
+        }
+    }
+}
