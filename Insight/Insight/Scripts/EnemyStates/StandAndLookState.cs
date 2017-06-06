08a2d9dc9bf6b45c2728ea6e6a2667d92e6a1c50
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-06-06 22:35:01 +0200

    added shooting with bullets and 2 states without behaviours

---
+++ Insight/Insight/Scripts/EnemyStates/StandAndLookState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insight.Engine;
+using Microsoft.Xna.Framework;
+
+namespace Insight.Scripts.EnemyStates
+{
+    class StandAndLookState : EnemyAIState
+    {
+        private float timer;
+
+        private float delay;
+
+        public override void EnterState(EnemyAI enemy)
+        {
+            timer = 0;
+            delay = 1;
+        }
+
+        public override void Execute(EnemyAI enemy)
+        {
+            if (timer >= delay)
+            {
+                enemy.gameObject.Transform.Rotation += new Vector3(0,1,0) * Time.deltaTime;
+                Debug.WriteLine(enemy.gameObject.Transform.forward);
+                timer = 0;
+            }
+
+            if (enemy.health <= 0)
+            {
+                enemy.ChangeState(new DeathState());
+            }
+        }
+
+        public override void Exit(EnemyAI enemy)
+        {
+            
+        }
+    }
+}
--- Insight/Insight/Scripts/RaycastTest.cs
+++ Insight/Insight/Scripts/RaycastTest.cs
@@ -22,15 +22,103 @@ namespace Insight.Scripts
 
         private GameObject test;
 
+
+
+        private PlayerBullets.Bullets? currentBulletLoaded;
+
         public RaycastTest(GameObject gameObject) : base(gameObject)
         {
+            currentBulletLoaded = null;
         }
 
         public override void Update()
         {
             MouseState ms = Mouse.GetState();
 
-            if (ms.LeftButton == ButtonState.Pressed && !isPressed)
+            #region LeftButton
+            if (ms.LeftButton == ButtonState.Pressed && !isPressed && currentBulletLoaded != null)
+            {
+                Vector3 minPoint = SceneManager.Instance.device.GraphicsDevice.Viewport.Unproject(
+                    new Vector3(SceneManager.Instance.device.GraphicsDevice.Viewport.Width / 2,
+                        SceneManager.Instance.device.GraphicsDevice.Viewport.Height / 2,
+                        0),
+                    SceneManager.Instance.currentScene.GetProjectionMatrix(),
+                    SceneManager.Instance.currentScene.GetMainCamera().view,
+                    Matrix.Identity);
+
+                Vector3 maxPoint = SceneManager.Instance.device.GraphicsDevice.Viewport.Unproject(
+                    new Vector3(SceneManager.Instance.device.GraphicsDevice.Viewport.Width / 2,
+                        SceneManager.Instance.device.GraphicsDevice.Viewport.Height / 2,
+                        1),
+                    SceneManager.Instance.currentScene.GetProjectionMatrix(),
+                    SceneManager.Instance.currentScene.GetMainCamera().view,
+                    Matrix.Identity);
+
+                Vector3 direction = maxPoint - minPoint;
+
+                direction.Normalize();
+
+                Debug.WriteLine(direction);
+                if (Physics.Raycast(minPoint, direction, out hit))
+                {
+                    isPressed = true;
+                    Physics.RaycastHit tempHit = hit;
+                    while (Vector3.Distance(minPoint, tempHit.point) < Vector3.Distance(minPoint, gameObject.Transform.Position + direction) && hit != null)
+                    {
+
+                        if (Physics.Raycast(tempHit.point + direction, direction, out hit))
+                        {
+                            tempHit = hit;
+                        }
+
+                    }
+                    if (hit != null)
+                    {
+                        //test = new GameObject(hit.point, false);
+                        //test.AddNewComponent<MeshRenderer>();
+
+                        //Effect e = SceneManager.Instance.Content.Load<Effect>("Shaders/PhongBlinnShader");
+                        //test.GetComponent<MeshRenderer>().Material = new DefaultMaterial(e);
+                        //test.GetComponent<MeshRenderer>().Load(SceneManager.Instance.Content);
+
+
+                        //Debug.WriteLine("Hit!" + " " + hit.collider.gameObject + " " + hit.distance);
+
+                        if (hit.collider.gameObject.physicLayer == Layer.Enemy)
+                        {
+                            hit.collider.gameObject.GetComponent<EnemyAI>().Hit(100);
+                        }
+                    }
+                }
+                switch (currentBulletLoaded)
+                {
+                    case PlayerBullets.Bullets.Agressive:
+                        gameObject.GetComponent<PlayerBullets>().aggresiveBullet = false;
+                        if (gameObject.GetComponent<PlayerBullets>().transmitterBullet)
+                        {
+                            currentBulletLoaded = PlayerBullets.Bullets.Transmitter;
+                        }
+                        break;
+                    case PlayerBullets.Bullets.Transmitter:
+                        gameObject.GetComponent<PlayerBullets>().transmitterBullet = false;
+                        if (gameObject.GetComponent<PlayerBullets>().aggresiveBullet)
+                        {
+                            currentBulletLoaded = PlayerBullets.Bullets.Agressive;
+                        }
+                        break;
+                }
+                isPressed = true;
+            }
+
+            if (ms.LeftButton == ButtonState.Released)
+            {
+                isPressed = false;
+            }
+            #endregion
+
+            #region RightButton
+
+            if (ms.RightButton == ButtonState.Pressed && !isPressed && currentBulletLoaded == PlayerBullets.Bullets.Transmitter)
             {
                 Vector3 minPoint = SceneManager.Instance.device.GraphicsDevice.Viewport.Unproject(
                     new Vector3(SceneManager.Instance.device.GraphicsDevice.Viewport.Width / 2,
@@ -76,14 +164,28 @@ namespace Insight.Scripts
                         test.GetComponent<MeshRenderer>().Load(SceneManager.Instance.Content);
 
 
-                        Debug.WriteLine("Hit!" + " " + hit.collider.gameObject + " " + hit.distance);
+                        //Debug.WriteLine("Hit!" + " " + hit.collider.gameObject + " " + hit.distance);
                     }
                 }
                 isPressed = true;
             }
 
-            if (ms.LeftButton == ButtonState.Released)
+            if (ms.RightButton == ButtonState.Released)
+            {
                 isPressed = false;
+            }
+
+            #endregion
+
+            KeyboardState keyState = Keyboard.GetState();
+            if (keyState.IsKeyDown(Keys.D1) && gameObject.GetComponent<PlayerBullets>().aggresiveBullet)
+            {
+                currentBulletLoaded = PlayerBullets.Bullets.Agressive;
+            }
+            if (keyState.IsKeyDown(Keys.D2) && gameObject.GetComponent<PlayerBullets>().aggresiveBullet)
+            {
+                currentBulletLoaded = PlayerBullets.Bullets.Transmitter;
+            }
             //base.Update();
         }
 
