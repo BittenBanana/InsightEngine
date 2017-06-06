aee49f1fc84e0eccc9069f5f04bf83a4a9c52821
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-06-06 21:08:32 +0200

    beginning of enemy AI

---
+++ Insight/Insight/Scripts/EnemyAI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insight.Engine;
+using Microsoft.Xna.Framework.Media;
+
+namespace Insight.Scripts
+{
+    class EnemyAI : BaseScript
+    {
+        public enum AiState
+        {
+            None,
+            WalkingToFirstPoint,
+            WalkingToSecondPoint,
+            FollowingPlayer,
+        }
+
+        private EnemyAIState currentState;
+
+        public EnemyAI(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public override void Update()
+        {
+            if (currentState != null)
+            {
+                currentState.Execute(this);
+            }
+            base.Update();
+        }
+
+        public void ChangeState(EnemyAIState newState)
+        {
+            currentState.Exit(this);
+            currentState = newState;
+            currentState.EnterState(this);
+        }
+    }
+}
---
+++ Insight/Insight/Scripts/EnemyAIState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insight.Scripts
+{
+    abstract class EnemyAIState
+    {
+        public abstract void EnterState(EnemyAI enemy);
+        public abstract void Execute(EnemyAI enemy);
+        public abstract void Exit(EnemyAI enemy);
+    }
+}
