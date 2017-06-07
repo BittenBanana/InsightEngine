da5c9e2ff8b5b067c67cc12f46eeb75a04786c9f
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-06-07 15:03:47 +0200

    added patrol state without behaviours

---
+++ Insight/Insight/Scripts/EnemyStates/PatrolState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insight.Scripts.EnemyStates
+{
+    class PatrolState : EnemyAIState
+    {
+        public override void EnterState(EnemyAI enemy)
+        {
+            Debug.WriteLine("Enter Patrol State");
+        }
+
+        public override void Execute(EnemyAI enemy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Exit(EnemyAI enemy)
+        {
+            Debug.WriteLine("Exit Patrol State");
+        }
+    }
+}
