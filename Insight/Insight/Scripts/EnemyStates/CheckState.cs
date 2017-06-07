97c94311b2ce1017cfc99d06c832ec89446f8202
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-06-07 19:46:16 +0200

    added check state with behaviours

--- Insight/Insight/Scripts/EnemyStates/CheckState.cs
+++ Insight/Insight/Scripts/EnemyStates/CheckState.cs
@@ -4,27 +4,45 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Insight.Engine;
 
 namespace Insight.Scripts.EnemyStates
 {
     class CheckState : EnemyAIState
     {
+        private float timer;
+        private float wait;
         public override void EnterState(EnemyAI enemy)
         {
+            timer = 0;
+            wait = 10;
             Debug.WriteLine("Enter Check State");
         }
 
         public override void Execute(EnemyAI enemy)
         {
-
+            if (EnemyWalkingSpots.getInstance()
+                    .DistanceFromDestination(enemy.gameObject.Transform.Position, enemy.enemySight.lastHeardPosition) >
+                0.1f)
+            {
+                EnemyWalkingSpots.getInstance()
+                    .MoveGameObjectToDestination(enemy.gameObject, enemy.enemySight.lastHeardPosition, 0.05f, 0.1f);
+            }
+            else
+            {
+                if (timer >= wait)
+                {
+                    if (!enemy.enemySight.isPlayerHeard)
+                    {
+                        enemy.ChangeState(enemy.defaultState);
+                    }
+                    timer = 0;
+                }
+                timer += Time.deltaTime;
+            }
             if (enemy.enemySight.isPlayerSeen)
             {
                 enemy.ChangeState(new ChaseState());
-            }
-
-            if (!enemy.enemySight.isPlayerHeard)
-            {
-                enemy.ChangeState(enemy.defaultState);
             }
         }
 
